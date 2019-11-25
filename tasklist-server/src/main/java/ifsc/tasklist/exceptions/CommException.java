78a0cadd8d5474eb5a3b05e1d2f86acaf388db64
Jhony Pereira <40416490+JPdesconectado@users.noreply.github.com>
2019-11-25 03:36:47 -0300

    Atualização do Java
    Mudando para o Java 12.

--- tasklist-server/src/main/java/ifsc/tasklist/exceptions/CommException.java
+++ tasklist-server/src/main/java/ifsc/tasklist/exceptions/CommException.java
@@ -1,5 +1,6 @@
 package ifsc.tasklist.exceptions;
 
+@SuppressWarnings("serial")
 public class CommException extends Exception {
 
 }
--- tasklist-server/src/main/java/ifsc/tasklist/exceptions/NetDeviceException.java
+++ tasklist-server/src/main/java/ifsc/tasklist/exceptions/NetDeviceException.java
@@ -1,5 +1,6 @@
 package ifsc.tasklist.exceptions;
 
+@SuppressWarnings("serial")
 public class NetDeviceException extends Exception {
 
 }
